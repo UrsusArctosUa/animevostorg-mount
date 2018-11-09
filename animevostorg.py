3c7da12d89f316e017ed27f6cf182051892ef64f
Dmytro Dubrovny <dubrovnyd@gmail.com>
2018-11-09 21:20:53 +0200

    Avaliable features:  - Latest directory in root dir  - list of folders in Latest, relevant to site pages with titles directories inside  - list of series as m3u8 files in each title forder with link to stream as content Also list of classes for using in realisations for other sites created.

---
+++ animevostorg.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+
+'''
+Created on Oct 30, 2018
+
+@author: Dmytro Dubrovny <dubrovnyd@gmail.com>
+'''
+from sitefs import RootDirectory, Directory, File, CacheControl, sanitize_filename, mount
+from bs4 import BeautifulSoup
+import requests
+import regex
+import json
+import sys
+
+
+class Reader(object):
+
+    def __init__(self):
+        self.web_url = 'http://animevost.org'
+        self.api_url = 'https://api.animevost.org/animevost/api/v0.2'
+
+    def list_latest(self):
+        page = requests.get(self.web_url)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        last_page = max([int(a.text) for a in soup.find(
+            attrs={"class": "block_4"}).findAll('a')])
+        return ["%03d" % page for page in range(1, ++last_page)]
+
+    def list_titles(self, page):
+        page = requests.get("%s/page/%d" % (self.web_url, page))
+        soup = BeautifulSoup(page.text, 'html.parser')
+        index = 0
+        for div in soup(attrs={"class": "shortstoryHead"}):
+            title = {'name': "%01d %s" % (index, sanitize_filename(div.a.text)), 'id': int(
+                regex.search('([0-9]+)[^/]+html', div.a.get('href'))[1])}
+            index += 1
+            yield title
+
+    def list_series(self, title_id):
+        page = requests.post("%s/playlist" % self.api_url,
+                             {'titleid': title_id}, None)
+        series = json.loads(page.text)
+        for item in series:
+            (number, name) = item['name'].split(' ')
+            if 'std' in item:
+                yield {'name': "%03d %s std" % (int(number), name), 'url': item['std']}
+            if 'hd' in item:
+                yield {'name': "%03d %s hd" % (int(number), name), 'url': item['hd']}
+
+
+class Item():
+    def __init__(self):
+        self.reader = Reader()
+
+
+class Root(RootDirectory):
+    def __init__(self):
+        RootDirectory.__init__(self)
+        self.children = [Latest()]
+
+
+class Latest(Directory, CacheControl, Item):
+    def __init__(self, ttl=20):
+        Directory.__init__(self, 'Latest')
+        CacheControl.__init__(self, ttl)
+        Item.__init__(self)
+
+    def get_children(self):
+        if not self.is_cached():
+            self.children = [Page(p) for p in self.reader.list_latest()]
+            self.validate_cache()
+        return self.children
+
+
+class Page(Directory, CacheControl, Item):
+    def __init__(self, page, ttl=60):
+        Directory.__init__(self, page)
+        CacheControl.__init__(self, ttl)
+        Item.__init__(self)
+        self.page = int(page)
+
+    def get_children(self):
+        if not self.is_cached():
+            self.children = [Title(t)
+                             for t in self.reader.list_titles(self.page)]
+            self.validate_cache()
+        return self.children
+
+
+class Title(Directory, CacheControl, Item):
+    def __init__(self, title, ttl=60):
+        Directory.__init__(self, title['name'])
+        CacheControl.__init__(self, ttl)
+        Item.__init__(self)
+        self.title = title
+
+    def get_children(self):
+        if not self.is_cached():
+            self.children = [
+                Record(r, self.get_name()) for r in self.reader.list_series(self.title['id'])]
+            self.validate_cache()
+        return self.children
+
+
+class Record(File, Item):
+    def __init__(self, record, title):
+        File.__init__(self, "%s.m3u8" % record['name'])
+        Item.__init__(self)
+        self.record = record
+        self.title = title
+
+    def get_content(self):
+        return str.encode("#EXTM3U\n#EXTINF:-1, %(title)s - %(name)s\n%(url)s\n"
+                          % {'url': self.record['url'], 'title': self.title,
+                              'name': self.record['name']})
+
+
+if __name__ == '__main__':
+    mount(sys.argv[1], Root(), 'animevostorg')
---
+++ sitefs.py
@@ -0,0 +1,119 @@
+'''
+Created on Oct 30, 2018
+
+@author: Dmytro Dubrovny <dubrovnyd@gmail.com>
+'''
+from fuse import Operations as FuseOperations, FuseOSError, FUSE
+import time
+import os
+import stat
+import errno
+
+
+def sanitize_filename(filename):
+    return filename.replace('/', "\u2571")
+
+
+class CacheControl():
+    def __init__(self, ttl):
+        self.cache = {'ttl': ttl, 'time': 0}
+
+    def is_cached(self):
+        return time.time() - self.cache['time'] < self.cache['ttl']
+
+    def validate_cache(self, ttl=None):
+        if ttl != None:
+            self.cache['ttl'] = ttl
+        self.cache['time'] = time.time()
+
+    def invalidate_cache(self):
+        self.cache['time'] = 0
+
+
+class FS(object):
+    def __init__(self, name):
+        self.name = name
+        self.attr = None
+
+    def get_attr(self):
+        return self.attr
+
+    def get_name(self):
+        return self.name
+
+    def get_content(self):
+        pass
+
+
+class Directory(FS):
+
+    def __init__(self, name):
+        FS.__init__(self, name)
+        self.attr = dict(st_atime=time.time(), st_ctime=time.time(), st_mtime=time.time(),
+                         st_gid=os.getgid(), st_uid=os.getuid(), st_mode=stat.S_IFDIR | 0o755,
+                         st_nlink=2, st_size=4096)
+        self.default_content = ['.', '..']
+        self.children = []
+
+    def get_children(self):
+        return self.children
+
+    def get_content(self):
+        return self.default_content + [c.get_name() for c in self.get_children()]
+
+
+class RootDirectory(Directory):
+    def __init__(self):
+        Directory.__init__(self, '')
+
+
+class File(FS):
+    def __init__(self, name):
+        FS.__init__(self, name)
+        self.attr = dict(st_atime=time.time(), st_ctime=time.time(), st_mtime=time.time(),
+                         st_gid=os.getgid(), st_uid=os.getuid(), st_mode=stat.S_IFREG | 0o644,
+                         st_nlink=1, st_size=4096)
+
+
+class Tree():
+    def __init__(self, root):
+        self.children = [root]
+
+    def get_children(self):
+        return self.children
+
+    def find_by_path(self, path):
+        splited_path = path.rstrip('/').split('/')
+
+        item = self
+        for name in splited_path:
+            dirs = item.get_children()
+            for d in dirs:
+                if d.get_name() == name:
+                    item = d
+                    break
+            else:
+                raise FuseOSError(errno.ENOENT)
+
+        return item
+
+
+class Operations(FuseOperations):
+
+    def __init__(self, root):
+        FuseOperations.__init__(self)
+        self.tree = Tree(root)
+
+    def getattr(self, path, fh=None):
+        return self.tree.find_by_path(path).get_attr()
+
+    def readdir(self, path, fh):
+        return self.tree.find_by_path(path).get_content()
+
+    def read(self, path, size, offset, fh):
+        return self.tree.find_by_path(path).get_content()
+
+
+def mount(mountpoint, root, name):
+    FUSE(Operations(root), mountpoint,
+         nothreads=True, foreground=True, fsname=name)
